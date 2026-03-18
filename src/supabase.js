bcd59c90a233ff04e3ad826f4fb3d9a92b28c929
fancesardev <fancesartrabajos@gmail.com>
2026-03-18 13:28:53 -0300

    corregir supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -4,10 +4,3 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-```
-
-**2.** Guardá el archivo y en la terminal ejecutá:
-```
-git add .
-git commit -m "usar variables de entorno para supabase"
-git push
